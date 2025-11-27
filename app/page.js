29d2faa1264aa18ae2151813848df047dda879ba
mehedi hasan <mhshohan04@gmail.com>
2025-11-27 11:23:54 +0600

    added Event section

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import EventList from "@/components/landing/EventList";
 import Header from "@/components/landing/Header";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
    <section className="container">
     <Header></Header>
+    <EventList></EventList>
    </section>
   );
 }
