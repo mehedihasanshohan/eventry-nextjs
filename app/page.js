50ed28485282ab818c57664e8c2af1d7e925e037
mehedi hasan <mhshohan04@gmail.com>
2025-11-27 11:11:56 +0600

    added header and navbar section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import Header from "@/components/landing/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <h2 className="text-red-400">welcome to nextjs .....</h2>
-    </div>
+   <section className="container">
+    <Header></Header>
+   </section>
   );
 }
