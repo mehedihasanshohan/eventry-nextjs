b4594bb8a6e1aa2b9cd7b7faec9032c8243d29af
mehedi hasan <mhshohan04@gmail.com>
2025-11-27 11:02:49 +0600

    projects setup done

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <h2 className="text-red-400">welcome to nextjs .....</h2>
+    </div>
+  );
+}
